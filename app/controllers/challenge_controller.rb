a4fba3796cfcc53b956f88c4d1710411dd113181
kyosuke awata <kyosuke.awata@maplesystems.co.jp>
2019-04-18 23:39:13 +0900

    勝者の表示

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -1,5 +1,5 @@
 class ChallengeController < ApplicationController
-  NUMBERS = (1..54).to_a
+  CARD_NUMBERS = (1..54).to_a
   def home
     @classification_1 = @classification_2 = '55'
   end
@@ -10,7 +10,27 @@ class ChallengeController < ApplicationController
   end
 
   def shuffle_card
-    @classification_1 = "%02d" % NUMBERS.shuffle.first
-    @classification_2 = "%02d" % NUMBERS.shuffle.first
+    @classification_1 = "%02d" % CARD_NUMBERS.shuffle.first
+    @classification_2 = "%02d" % CARD_NUMBERS.shuffle.first
+    @number = {}
+    j = 1
+    CARD_NUMBERS.each do |i|
+      j = 1 if j == 14
+      @number["#{i}"] = j
+      i += 1
+      j += 1
+    end
+    @number["53"] = 14
+    @number["54"] = 14
+    tmp_1 = @number["%01d" % @classification_1]
+    tmp_2 = @number["%01d" % @classification_2]
+    if tmp_1 == tmp_2
+      @winner = 3
+    elsif tmp_1 > tmp_2
+      @winner = 1
+    else
+      @winner = 2
+    end
   end
 end
+
